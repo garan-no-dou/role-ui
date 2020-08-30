9af8ae00a524a689328ccf54e4a4b76b2c11f9f7
mduenast <ratchetmdt@gmail.com>
2020-08-30 16:22:37 +0200

    Add Login form and google sso component.

--- src/components/GoogleSignin/GoogleSignin.js
+++ src/components/GoogleSignin/GoogleSignin.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './GoogleSignin.module.css';
 import GoogleAuthenticationDTO from "./GoogleAuthenticationDTO";
+import {MDBBtn, MDBCol, MDBContainer, MDBInput, MDBJumbotron, MDBRow} from "mdbreact";
+import GoogleBtn from "../GoogleBtn/GoogleBtn";
 
 class GoogleSignin extends React.Component {
 
@@ -11,34 +13,44 @@ class GoogleSignin extends React.Component {
     render() {
         return (
             <div className={styles.Signin} data-testid="Signin">
-                <div className="g-signin2" data-onsuccess="{this.onSignIn}" data-theme="dark"/>
+                <MDBContainer>
+                    <MDBJumbotron className="m-5">
+                        <MDBRow>
+                            <MDBCol>
+                                <form>
+                                    <MDBRow>
+                                        <MDBCol>
+                                            <p className="h2 text-center mb-4">Role Online App</p>
+                                            <p className="h5 text-center mb-4">Sign in</p>
+                                            <div className="grey-text">
+                                                <MDBInput label="Type your email" icon="envelope" group type="email"
+                                                          validate
+                                                          error="wrong"
+                                                          success="right" />
+                                                <MDBInput label="Type your password" icon="lock" group type="password"
+                                                          validate/>
+                                            </div>
+                                        </MDBCol>
+                                    </MDBRow>
+                                    <MDBRow>
+                                        <MDBCol>
+                                            <div className="text-center">
+                                                <MDBBtn>Login</MDBBtn>
+                                            </div>
+                                        </MDBCol>
+                                    </MDBRow>
+                                    <MDBRow>
+                                        <MDBCol>
+                                            <GoogleBtn />
+                                        </MDBCol>
+                                    </MDBRow>
+                                </form>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBJumbotron>
+                </MDBContainer>
+
             </div>
-        )
-    }
-
-    onSignIn(googleUser) {
-        // Useful data for your client-side scripts:
-        let profile = googleUser.getBasicProfile();
-
-        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-        console.log('Full Name: ' + profile.getName());
-        console.log('Given Name: ' + profile.getGivenName());
-        console.log('Family Name: ' + profile.getFamilyName());
-        console.log("Image URL: " + profile.getImageUrl());
-        console.log("Email: " + profile.getEmail());
-
-        // The ID token you need to pass to your backend:
-        let authResponse = googleUser.getAuthResponse;
-        var id_token = authResponse().id_token;
-        console.log("ID Token: " + id_token);
-
-        let googleAuthentication = new GoogleAuthenticationDTO(
-            authResponse.access_token,
-            authResponse.id_token,
-            authResponse.scope,
-            authResponse.expires_in,
-            authResponse.first_issued_at,
-            authResponse.expires_at
         )
     }
 }
